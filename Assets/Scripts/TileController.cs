915d108e1ece8faaead180d5b34b196a1ec9076b
Josue Cortina Suarez <jcortina781@gmail.com>
2017-07-29 01:38:59 -0700

    Add a background for the tiles and the "checked" and "wrong" images

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -6,11 +6,17 @@ public class TileController : MonoBehaviour
 {
     public int row;
     public int col;
+
     private SpriteRenderer sprite;
+    private Color originalColor;
+    private TilesManagerController tilesManager;
+    private SpriteRenderer stateImage;
 
     void Awake()
     {
         sprite = GetComponent<SpriteRenderer> ();
+        stateImage = transform.GetChild (0)
+            .GetComponent<SpriteRenderer> ();
     }
 	
     // Update is called once per frame
@@ -19,10 +25,23 @@ public class TileController : MonoBehaviour
 		
     }
 
-    public void SetUp(int i, int j, Color[] colors)
+    public void SetUp(int i, int j, Color[] colors, TilesManagerController tilesManager)
     {
         row = i;
         col = j;
         sprite.color = colors.Choice ();
+        originalColor = sprite.color;
+        this.tilesManager = tilesManager;
+    }
+
+    void OnMouseDown()
+    {
+        print (string.Format ("Hello from tile ({0}, {1})", row, col));
+        stateImage.sprite = tilesManager.rightImage;
+    }
+
+    void OnMouseExit()
+    {
+        stateImage.sprite = null;
     }
 }
