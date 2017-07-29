8b708bba272beed0e2c1401c39dde80480ad9f1f
Josue Cortina Suarez <jcortina781@gmail.com>
2017-07-28 22:43:04 -0700

    Create the tiles prefab, a manager for them and add empty controllers for both

---
+++ Assets/Scripts/TilesManagerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesManagerController : MonoBehaviour
+{
+    public int tilesRows = 4;
+    public int tilesCols = 4;
+
+    public Vector3 tilesPivot = Vector3.zero;
+    public int tilesScale = 70;
+    public int tilesSpacing = 15;
+    public GameObject tilesPrefab;
+
+    private TileController[,] tiles;
+
+    void Awake()
+    {
+        tiles = new TileController[tilesRows, tilesCols];
+
+        for (int i = 0, wi = 0; i < tilesRows; i++, wi += tilesSpacing) {  // wi: world i (row in world space)
+            for (int j = 0; j < tilesCols; j++) {
+                tiles [i, j] = GameObject.Instantiate (tilesPrefab, this.transform).GetComponent<TileController> ();
+                tiles [i, j].transform.localPosition = new Vector3 ();
+            }
+        }
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+    void BuildTiles()
+    {
+		
+    }
+}
