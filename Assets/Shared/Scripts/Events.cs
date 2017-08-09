f8050396f0ceeebccb0b90827985bee290ec35ba
Josue Cortina Suarez <jcortina781@gmail.com>
2017-08-08 23:50:45 -0700

    Upgrade the concept of 'graphics-logic API' to a general purpose event-driven mecanism
    The change came because, for instance, a logical event
    can be connected to logical callbacks besides the graphical
    ones. Of course, the older events are a subset of the newer.
    This way is more flexible but, maybe, harder to debug and
    trace. Now, there are two static classes, Logic and Graphics,
    the former contains the logical events while the latter
    contains the graphical events. Both classes are defined
    inside the namespace Events.

---
+++ Assets/Shared/Scripts/Events.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace KoreKrush
+{
+    namespace Events
+    {
+        public static class Logic
+        {
+            public static event Action 
+                GameStarted;
+            public static event Action<TileController>
+                TileSpawned,
+                TileHovered,
+                TileSelected,
+                TileConnected,
+                TileDisconnected,
+                TileDisplaced,
+                TileDestroyed;
+            public static event Action<List<TileController>>
+                TilesSequenceStarted,
+                TilesSequenceCompleted;
+        }
+
+        public static class Graphics
+        {
+            
+        }
+    }
+}
