9056835ee7a578ce0192dbfba222bde79c6891d5
Josue Cortina Suarez <jcortina781@gmail.com>
2017-08-10 20:34:42 -0700

    Transform events into delegates
    This is beacuse an event just can be used on the left side of a += or -=
    outside of the class in which it is defined, so it does not fit our needs.

--- Assets/Logic/Scripts/TilesManagerController.cs
+++ Assets/Logic/Scripts/TilesManagerController.cs
@@ -15,37 +15,43 @@ public class TilesManagerController : MonoBehaviour
     public Sprite wrongImage;
 
     private TileController[,] tiles;
-    private List<TileController> selectedTiles = new List<TileController> ();
+    private List<TileController> selectedTiles = new List<TileController>();
     private Transform tilesContainer;
     private LineRenderer selectionLine;
 
     void Awake()
     {
-        tilesContainer = transform.GetChild (0);
-        selectionLine = GetComponent<LineRenderer> ();
+        tilesContainer = transform.GetChild(0);
+        selectionLine = GetComponent<LineRenderer>();
         tiles = new TileController[tilesRows, tilesCols];
     }
 
     void Start()
     {
-        BuildTiles ();
+        BuildTiles();
     }
 	
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp (0)) {
-            if (selectedTiles.Count == 1) {
-                selectedTiles [0].Unselect ();
-            } else {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (selectedTiles.Count == 1)
+            {
+                selectedTiles[0].Unselect();
+            }
+            else
+            {
                 for (int i = 0; i < selectedTiles.Count; i++)
-                    selectedTiles [i].ChangeColor (tilesColors.Choice ());
+                    selectedTiles[i].ChangeColor(tilesColors.Choice());
                 selectionLine.positionCount = 0;
             }
-            selectedTiles.Clear ();
-            for (int i = 0; i < tilesRows; i++) {
-                for (int j = 0; j < tilesCols; j++) {
-                    tiles [i, j].Unselect ();
+            selectedTiles.Clear();
+            for (int i = 0; i < tilesRows; i++)
+            {
+                for (int j = 0; j < tilesCols; j++)
+                {
+                    tiles[i, j].Unselect();
                 }
             }
         }
@@ -56,57 +62,68 @@ public class TilesManagerController : MonoBehaviour
         float row, col;  // row and col in world space
 
         col = -tilesSpacing * (tilesRows / 2) + (tilesRows % 2 == 0 ? tilesSpacing / 2f : 0);
-        for (int i = 0; i < tilesRows; i++, col += tilesSpacing) {
+        for (int i = 0; i < tilesRows; i++, col += tilesSpacing)
+        {
             row = -tilesSpacing * (tilesCols / 2) + (tilesCols % 2 == 0 ? tilesSpacing / 2f : 0);
-            for (int j = 0; j < tilesCols; j++, row += tilesSpacing) {
-                tiles [i, j] = GameObject.Instantiate (tilesPrefab, tilesContainer)
-                    .GetComponent<TileController> ();
-                tiles [i, j].SetUp (i, j, tilesColors, this);
-                tiles [i, j].transform.localPosition = new Vector3 (row, col);
+            for (int j = 0; j < tilesCols; j++, row += tilesSpacing)
+            {
+                tiles[i, j] = GameObject.Instantiate(tilesPrefab, tilesContainer)
+                    .GetComponent<TileController>();
+                tiles[i, j].SetUp(i, j, tilesColors, this);
+                tiles[i, j].transform.localPosition = new Vector3(row, col);
             }
         }
     }
 
     private bool AdjacentTiles(TileController tile1, TileController tile2)
     {
-        return Mathf.Abs (tile1.row - tile2.row) + Mathf.Abs (tile1.col - tile2.col) == 1
-        || Mathf.Abs (tile1.row - tile2.row) == 1 && Mathf.Abs (tile1.col - tile2.col) == 1;
+        return Mathf.Abs(tile1.row - tile2.row) + Mathf.Abs(tile1.col - tile2.col) == 1
+        || Mathf.Abs(tile1.row - tile2.row) == 1 && Mathf.Abs(tile1.col - tile2.col) == 1;
     }
 
     public void BeginTilesSelection(TileController firstTile)
     {
-        Assert.IsTrue (selectedTiles.Count == 0, "The list must be empty to begin the selection");
-        selectedTiles.Add (firstTile);
+        Assert.IsTrue(selectedTiles.Count == 0, "The list must be empty to begin the selection");
+        selectedTiles.Add(firstTile);
     }
 
     public bool TryAddTile(TileController tile)
     {
-        if (selectedTiles.Count == 0) {
-            BeginTilesSelection (tile);
+        if (selectedTiles.Count == 0)
+        {
+            BeginTilesSelection(tile);
             return true;
         }
 
-        var lastTile = selectedTiles.Last ();
-        if (tile.SameColor (lastTile)) {
-            if (tile.selected) {
-                if (selectedTiles.Count > 1 && tile == selectedTiles [selectedTiles.Count - 2]) {
-                    lastTile.Unselect ();
-                    selectedTiles.RemoveAt (selectedTiles.Count - 1);
+        var lastTile = selectedTiles.Last();
+        if (tile.SameColor(lastTile))
+        {
+            if (tile.selected)
+            {
+                if (selectedTiles.Count > 1 && tile == selectedTiles[selectedTiles.Count - 2])
+                {
+                    lastTile.Unselect();
+                    selectedTiles.RemoveAt(selectedTiles.Count - 1);
                     if (selectedTiles.Count == 1)
                         selectionLine.positionCount = 0;
                     else
                         selectionLine.positionCount--;
                 }
                 return true;
-            } else if (AdjacentTiles (tile, lastTile)) {
-                selectedTiles.Add (tile);
-                if (selectedTiles.Count == 2) {
+            }
+            else if (AdjacentTiles(tile, lastTile))
+            {
+                selectedTiles.Add(tile);
+                if (selectedTiles.Count == 2)
+                {
                     selectionLine.positionCount = 2;
                     for (int i = 0; i < 2; i++)
-                        selectionLine.SetPosition (i, selectedTiles [i].transform.position + new Vector3 (0, 0, -5));    
-                } else {
+                        selectionLine.SetPosition(i, selectedTiles[i].transform.position + new Vector3(0, 0, -5));    
+                }
+                else
+                {
                     selectionLine.positionCount++;
-                    selectionLine.SetPosition (selectedTiles.Count - 1, tile.transform.position + new Vector3 (0, 0, -5));
+                    selectionLine.SetPosition(selectedTiles.Count - 1, tile.transform.position + new Vector3(0, 0, -5));
                 }
                 return true;
             }
--- Assets/Shared/Scripts/Events.cs
+++ Assets/Shared/Scripts/Events.cs
@@ -10,19 +10,20 @@ namespace KoreKrush
     {
         public static class Logic
         {
-            public static event Action 
-                GameStarted;
-            public static event Action<TileController>
-                TileSpawned,
-                TileHovered,
-                TileSelected,
-                TileConnected,
-                TileDisconnected,
-                TileDisplaced,
-                TileDestroyed;
-            public static event Action<List<TileController>>
-                TilesSequenceStarted,
-                TilesSequenceCompleted;
+            public static Action 
+                GameStarted;  // well... the game just... started
+            public static Action<TileController>
+                TileSpawned, // a tile appears on the board
+                TileHovered, // the cursor passed over a tile
+                TileSelected, // the user clicked a tile
+                TileConnected, // a new tile joined to the sequence
+                TileDisconnected, // the last tile on the sequence was removed from it
+                TileDestroyed;  // a tile was removed from the board
+            public static Action<TileController, int, int>
+                TileDisplaced;  // a tile was moved from one to another position
+            public static Action<List<TileController>>
+                TilesSequenceStarted, // a new sequence of tiles has begun
+                TilesSequenceCompleted;  // the user chose to finish the current sequence
         }
 
         public static class Graphics
