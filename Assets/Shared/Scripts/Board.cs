eab3a54e97c6b4736e47f4d9a03efe83adf992e8
Josue Cortina Suarez <jcortina781@gmail.com>
2017-08-12 00:37:56 -0700

    Wrap the board of tiles and selected tiles in a single static class

---
+++ Assets/Shared/Scripts/Board.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KoreKrush
+{
+    public static class Board
+    {
+        public static TileController[,] tiles;
+        public static List<TileController> tilesSequence;
+        public static int numberOfColors;
+
+        public static int Rows
+        {
+            get { return tiles.GetLength(0); }
+        }
+
+        public static int Cols
+        {
+            get { return tiles.GetLength(1); }
+        }
+
+        public static TileController Last
+        {
+            get { return tilesSequence.Count > 0 ? tilesSequence[tilesSequence.Count - 1] : null; }
+            set 
+            {
+                if (value) 
+                    tilesSequence.Add(value);
+                else 
+                    tilesSequence.RemoveAt(tilesSequence.Count - 1); 
+            }
+        }
+
+        public static TileController SecondLast
+        {
+            get { return tilesSequence.Count > 1 ? tilesSequence[tilesSequence.Count - 2] : null; }
+        }
+    }
+}
