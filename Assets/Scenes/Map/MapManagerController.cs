c2067057dc240ddfbbfa062f175c20101c69cdc7
Josue Cortina Suarez <jcortina781@gmail.com>
2017-09-14 00:30:51 -0700

    Add a clickable area to level Collision and create the Map Manager

---
+++ Assets/Scenes/Map/MapManagerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+
+public class MapManagerController : MonoBehaviour
+{
+    public RawImage splash;
+
+    // Use this for initialization
+    void Start()
+    {
+        var original = splash.color;
+        var c = original;
+        c.a = 1;
+        splash.color = c;
+
+        splash.DOColor(original, 1);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        splash.DOColor(new Color(0, 0, 0, 1), 1)
+            .OnComplete(() => SceneManager.LoadScene(levelName));
+    }
+}
