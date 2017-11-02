bd2497b4f518ae7cc80ca644dc1f22db4a7e2840
Josue Cortina Suarez <jcortina@gmail.com>
2017-11-02 12:10:54 -0700

    Create a simple hierarchy of effects decoupled from Tiles

---
+++ Assets/Logic/Scripts/Effects/BaseEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+public abstract class BaseEffect : MonoBehaviour
+{
+    public abstract void Apply();
+}
+
+public abstract class PreEffect : BaseEffect
+{
+    
+} 
+
+public abstract class RemovableEffect : BaseEffect
+{
+    public abstract void Remove();
+}
+
+public abstract class OnConnectEffect : RemovableEffect
+{
+    
+}
+
+public abstract class OnDestroyEffect : BaseEffect
+{
+    
+}
