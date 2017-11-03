fe322021e9cf69e49df875d59453332ccf1b0bb6
Josue Cortina Suarez <jcortina@gmail.com>
2017-11-02 21:32:21 -0700

    Unify Tiles logic and graphics

--- Assets/Logic/Scripts/TileController.cs
+++ Assets/Logic/Scripts/TileController.cs
@@ -12,13 +12,35 @@ public class TileController : MonoBehaviour
     public int Row { get { return cell.row; } }
     public int Col { get { return cell.col; } }
     public bool IsMovable { get { return true; } }
+    
+    private SpriteRenderer stateImage;
 
-    void OnMouseDown()
+    public SpriteRenderer Sprite { get; private set; }
+
+    public Color Color
+    {
+        get { return Sprite.color; }
+        set { Sprite.color = value; }
+    }
+
+    public Sprite StateImage 
+    {
+        set { stateImage.sprite = value; }
+    }
+
+    private void Awake()
+    {
+        Sprite = GetComponent<SpriteRenderer>();
+        stateImage = transform.GetChild(0)
+            .GetComponent<SpriteRenderer>();
+    }
+    
+    private void OnMouseDown()
     {
         KoreKrush.Events.Logic.TileSelect_L(this);
     }
 
-    void OnMouseEnter()
+    private void OnMouseEnter()
     {
         if (Input.GetMouseButton(0))
             KoreKrush.Events.Logic.TileSelect_L(this);
